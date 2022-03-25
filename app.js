fca58ed52ca75ade9465fc0875efdc5db2791573
leesh2347 <81743960+leesh2347@users.noreply.github.com>
2022-03-26 02:03:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const Discord=require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES
+  ]
+});
 const fs = require('fs');
 const prefix = '';
 var i;
